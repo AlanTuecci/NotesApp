3a8a4edb818db85f076dbe59151a870a7d77eb2e
AlanTuecci <93105057+AlanTuecci@users.noreply.github.com>
2024-07-24 17:35:27 -0400

    Version 0.2

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,19 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const app = express();
-const collegeDirInfoRouter = require("./routes/college-router/dir-info-router.js")
+const cors = require("cors");
+const collegeDirAccessRouter = require("./routes/college-router/dir-access-router.js")
 const collegeClassFileAccessRouter = require("./routes/college-router/file-download-router.js")
 
 //C:\School\College\Junior College\Spring Semester\CSCI 49375
 //C:\School\NotesApp\backend
 
-app.use("/API/College", collegeDirInfoRouter);
+app.use(express.static("../frontend/build"))
+app.use(cors());
+app.use("/API/DirAccess/College", collegeDirAccessRouter);
 app.use("/API/FileAccess/College", collegeClassFileAccessRouter);
 
 
-app.listen(3000, (req, res) => {
-  console.log("Server listening on port 3000!");
+app.listen(3001, (req, res) => {
+  console.log("Server listening on port 3001!");
 });
